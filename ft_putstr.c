09a6ab3883be5c4b7ef034e5425ff10fbafa43ba
kayumi <kimuraayumi70@gmail.com>
2021-12-10 03:03:32 +0900

    89

--- ft_putstr.c
+++ ft_putstr.c
@@ -8,9 +8,9 @@ ssize_t ft_putstr(char *n)
         i = 0;
         if (n == NULL)
         {
-                write(1, "(null)", 6);
+                return (write(1, "(null)", 6));
         }
-        while (*n)
+        while (*n != '\0')
         {
                 i++;
                 write(1, n, 1);
--- test.c
+++ test.c
@@ -2,12 +2,8 @@
 
 int main(void)
 {
-	int  i;
-
-	i = 0;
-	i = printf("%d\n", 0);
-	printf("%d\n", i);
-	i = ft_printf("%d\n", 0);
-	printf("%d\n", i);
+	printf ("%d\n" , LONG_MIN);
+	ft_printf ("%d\n" , LONG_MIN);
 	return (0);
+
 }
