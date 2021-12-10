c7e4f6645073fe1db18fb35cff03bba84a14e5f0
kayumi <kimuraayumi70@gmail.com>
2021-12-10 16:12:54 +0900

    98

--- ft_printf.c
+++ ft_printf.c
@@ -1,61 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kayumi <kayumi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 12:44:19 by kayumi            #+#    #+#             */
+/*   Updated: 2021/12/10 16:11:58 by kayumi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int ft_printf(const char *s, ...)
+int	another_function(char *max, va_list *ap)
 {
-	char *max;
-	ssize_t len;
-	va_list ap;
-
-	max = (char *)s;
-	va_start (ap, s);
+	ssize_t	len;
 
 	len = 0;
+	if (*max == 'p')
+	{
+		len += ft_putstr("0x");
+		len += putnbr_base(va_arg(*ap, unsigned long long), OX);
+	}
+	if (*max == 'u')
+	len += putnbr_base(va_arg(*ap, unsigned int), NUM);
+	else if (*max == 'x')
+	len += putnbr_base(va_arg(*ap, unsigned int), OX);
+	else if (*max == 'X')
+	len += putnbr_base(va_arg(*ap, unsigned int), O_X);
+	else if (*max == 'd' || *max == 'i')
+	len += ft_putnbr(va_arg(*ap, int));
+	else if (*max == 's')
+	len += ft_putstr(va_arg(*ap, char *));
+	else if (*max == '%')
+	len += ft_putchar('%');
+	return (len);
+}
+
+int	ft_printf(const char *s, ...)
+{
+	char	*max;
+	ssize_t	len;
+	va_list	ap;
+
+	max = (char *)s;
+	va_start(ap, s);
+	len = 0;
+	if (len >= INT_MAX)
+		return (-1);
 	while (*max != '\0')
 	{
 		if (*max != '%')
-		{
 			len += ft_output(&max);
-		}
 		else if (*max == '%')
 		{
 			max++;
 			if (*max == 'c')
-			{
 				len += ft_putchar((unsigned char)va_arg(ap, int));
-			}
-			else if (*max == 's')
-			{
-				len += ft_putstr(va_arg(ap, char *));
-			}
-			else if (*max == 'p')
-			{
-				len += ft_putstr("0x");
-				len += putnbr_base(va_arg(ap, unsigned long long), "0123456789abcdef");
-			}
-			else if (*max == 'd' || *max == 'i')
-			{
-				len += ft_putnbr(va_arg(ap, int));
-			}
-			else if (*max == 'u')
-			{
-				len += putnbr_base(va_arg(ap, unsigned int), "0123456789");
-			}
-			else if (*max == 'x')
-			{
-				len += putnbr_base(va_arg(ap, unsigned int), "0123456789abcdef");
-			}
-			else if (*max == 'X')
-			{
-				len += putnbr_base(va_arg(ap, unsigned int), "0123456789ABCDEF");
-			}
-			else if (*max == '%')
-			{
-				len += ft_putchar('%');
-			}
+			else
+				len += another_function(max, &ap);
 			++max;
 		}
-		if (len >= INT_MAX)
-			return (-1);
 	}
 	va_end(ap);
 	return (len);
--- ft_printf.h
+++ ft_printf.h
@@ -1,6 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kayumi <kayumi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 13:26:50 by kayumi            #+#    #+#             */
+/*   Updated: 2021/12/10 13:39:26 by kayumi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-
+# define OX "0123456789abcdef"
+# define NUM "0123456789"
+# define O_X "0123456789ABCDEF"
 # include <limits.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -8,15 +22,15 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int     ft_printf(const char *s, ...);
-int ft_output(char **max);
-ssize_t ft_putstr(char *n);
-ssize_t ft_putchar(char c);
-ssize_t ft_evolution_putnbr(size_t n);
-ssize_t  ft_putnbr(int n);
-size_t count_decimal (int number);
+int	ft_printf(const char *s, ...);
+int	ft_output(char **max);
+ssize_t	ft_putstr(char *n);
+ssize_t	ft_putchar(char c);
+ssize_t	ft_evolution_putnbr(size_t n);
+ssize_t	ft_putnbr(int n);
+size_t	count_decimal(int number);
 ssize_t	putnbr_base(unsigned long long n, char *base);
-size_t count_function (int number);
+size_t	count_function(int number);
 size_t	ft_strlen(char const *s);
-
+int	another_function(char *max, va_list *ap);
 #endif
